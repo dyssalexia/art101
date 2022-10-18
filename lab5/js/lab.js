9b7128a617995ce4c29702051db50f80d2175b5d
dyssalexia <114507347+dyssalexia@users.noreply.github.com>
2022-10-18 08:10:04 -0700

    lab 5

---
+++ lab5/js/lab.js
@@ -0,0 +1,21 @@
+/*
+ * Author:   Alex Jones <aljujone@ucsc.edu.
+ * Created:   18.10.2023
+ * License: Public Domain
+ */
+
+// Variable Definition
+make = "Ford"
+model = "Escape"
+color = "White"
+year = "2017"
+
+// Age Calculation
+age = 2022 - year;
+
+// Output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + "<br>");
