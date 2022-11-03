61a5c22b490ad48775b2ec556bd4a705653349d3
dyssalexia <114507347+dyssalexia@users.noreply.github.com>
2022-11-02 18:40:19 -0700

    lab9

---
+++ lab10/js/lab.js
@@ -0,0 +1,27 @@
+// Author : Alexia Jones
+// Date: 11.03.22
+// Email: aljujone@ucsc.edu
+
+// Borrow the nameSort()function from Lab 7
+function scrambleUserName(inputName) {
+  var userName = inputName;
+
+  var nameArray = userName.split('');
+
+  var sortedArray = nameArray.sort();
+
+  var sortedName = sortedArray.join("");
+
+  return sortedName;
+}
+// Attach an event listener to your button that gets the value of the niput field, Runs that value through your sort() or anagram() function and saves the results.
+
+var buttonEl = document.getElementById("my-button");
+
+var outputEl = document.getElementById("output");
+
+buttonEl.addEventListener('click', function() {
+  var inputName= document.getElementById("user-name").value
+  var sortedName = scrambleUserName(inputName);
+  outputEl.innerHTML = sortedName;
+})
